8da633ac18c38bc6db9dcc61c4ae01c92f172d58
taniaPC <tania.sarmientomnz@uanl.edu.mx>
2025-01-27 13:28:02 -0600

    Script 1 de prueba potencial de R

---
+++ Script_1.R
@@ -0,0 +1,31 @@
+# Tania Isela Sarmiento Muñoz
+# 27/01/2025
+# Matr: 1503831
+#
+
+
+# Datos básicos -----------------------------------------------------------
+3 + 5 
+8 * 4
+3 / 2 
+
+suma <- 3 + 5
+suma
+suma * 4
+
+suma * suma #Si funciona por usar de forma correcta el nombre
+
+Suma * suma #Forma incorrecta, no existe Suma (S)
+
+
+# Importar datos ----------------------------------------------------------
+
+edad <- c(50, 25, 30, 18, 45, 25, 36, 17, 45, 24, 
+          22, 18, 41, 43)
+edad
+mean (edad)
+var(edad)
+sd(edad)
+edad * 2
+edad * c(2, 3)
+
